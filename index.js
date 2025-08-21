3f58feff44106e41b6671ed2eb3d7dcb01f0577a
Muhammad Rehan <rehanbhaifromsdk@gmail.com>
2025-08-20 23:05:45 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+var todo = [];
+           var docu = document.getElementById("add")
+           var up = document.getElementById("update")
+ 
+           
+           var ad = document.getElementById("todo-iput")
+           ad.style.display="none"
+
+           function disply(){
+            ad.style.display="flex"
+           }
+
+  color()        
+             function color(){
+            docu.style.display = "block"
+            up.style.display ="none"
+             }
+function addlist() {
+  var add = document.getElementById("input");
+  if (add.value === "") {
+    alert("Enter some........!");
+    return;
+  }
+  todo.push(add.value);
+  add.value = "";
+  showlist();
+}
+function showlist() {
+  var showlist = document.getElementById("show");
+  showlist.innerHTML = "";
+  for (var i = 0; i < todo.length; i++) {
+    showlist.innerHTML += `<div class="g"><div class="gt"><li onclick="update(${i})">${todo[i]}</li></div><button class="gun" onclick="delet(${i})">delet</button></div>`;
+  }
+}
+function update(h) {
+  var doc = document.getElementById("input");
+  doc.value = todo.splice(h, 1);
+  showlist()
+     docu.style.display = "none"
+     up.style.display ="block"
+     
+  }
+
+function delet(index) {
+  todo.splice(index, 1);
+  showlist();
+}
